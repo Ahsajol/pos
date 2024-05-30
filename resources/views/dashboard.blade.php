52c0b7538f2d8950f3d9e011419ded2048dfa39c
Akram Hossain <ahsajol62@gmail.com>
2024-05-30 20:57:51 +0600

    auth added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,47 +1,17 @@
-@extends('layout')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="container-fluid p-5 bg-primary text-white text-center">
-                    <h1>Dashboard</h1>
-                    <div class="container mt-5">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="card bg-info">
-                                    <div class="card-body">
-                                        <h4 class="card-title">Todays Sales</h4>
-                                        <p class="card-text">Some example text. Some example text.</p>
-                                        <a href="#" class="card-link">Card link</a>
-                                        <a href="#" class="card-link">Another link</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="card bg-info">
-                                    <div class="card-body">
-                                        <h4 class="card-title">Todays Cost</h4>
-                                        <p class="card-text">Some example text. Some example text.</p>
-                                        <a href="#" class="card-link">Card link</a>
-                                        <a href="#" class="card-link">Another link</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="card bg-info">
-                                    <div class="card-body">
-                                        <h4 class="card-title">More</h4>
-                                        <p class="card-text">Some example text. Some example text.</p>
-                                        <a href="#" class="card-link">Card link</a>
-                                        <a href="#" class="card-link">Another link</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
-@endsection
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\ProductController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -17,20 +14,18 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('', function () {
-    return view('dashboard');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('dashboard', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('/category', CategoryController::class);
-
-Route::resource('/brand', BrandController::class);
-
-Route::resource('/product', ProductController::class);
+require __DIR__ . '/auth.php';
